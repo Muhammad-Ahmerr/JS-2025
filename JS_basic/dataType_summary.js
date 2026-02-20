c9b36a920ffbce530335eeebf7bfd768c5951ec6
Muhammad-Ahmerr <126159056+Muhammad-Ahmerr@users.noreply.github.com>
2026-02-20 17:56:20 +0000

    dataSummary

---
+++ JS_basic/dataType_summary.js
@@ -0,0 +1,39 @@
+// the 2 ways in which the data store and access in memory like call by function/call by Reference  
+//#Premitive   (call by Value) it means whenever you use it data it will be as copy not actuall reference then every changes will be copy 
+
+//Also JavaScript is a dynamically typed language because we didn't provide its type like String
+
+//7-types:
+//String, Number, boolean, bigInt, Symbol, undefined,null
+
+const id=Symbol("123")
+const anotherID=Symbol("123")
+
+// console.log(id==anotherID);
+
+
+//#NonPremitive (Reference Type)
+//Array, Objects and functions
+
+
+const data =["phislosophy", "marval", 123, true]
+
+
+// console.log(data[0]);
+
+let myObj={
+    name: "Ahmer",
+    age: "27",
+    city:"Multan"    
+}
+
+// console.log(myObj["name"]);
+
+
+let ahm= function(){
+
+
+}
+
+//if you want to become a JS expert then you need to know the JS Objects and web events
+
