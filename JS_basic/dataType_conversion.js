2c35fab5205e4a2cfffc81c1bdb78182be10f884
Muhammad-Ahmerr <126159056+Muhammad-Ahmerr@users.noreply.github.com>
2026-02-20 12:34:00 +0000

    conversion of dataType

---
+++ JS_basic/dataType_conversion.js
@@ -0,0 +1,32 @@
+let score= "22aab"
+
+// console.log(typeof score);
+
+let changeAmount= Number(score)   //it convert "22aab" in number that's the main issue we use typeScript also in case of null it converted into 0 that will be the error in temp.
+
+// console.log(typeof changeAmount);
+// console.log(changeAmount);
+
+
+// "33" => 33
+//"33qba" => Number but NAN
+//null => Number but 0
+//undefined => 
+
+let isLoggedIn= 1
+
+let boleanchecked= Boolean(isLoggedIn)
+// console.log(typeof boleanchecked);
+
+// console.log(boleanchecked);
+
+// "" => false
+//"ssas" => true
+
+
+let myNum= 22
+
+let changeNum= String( myNum)
+console.log(typeof changeNum);
+
+
