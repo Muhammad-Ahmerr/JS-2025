6b6dbcab8b02e675fb4aa824094469545a693f4b
Muhammad-Ahmerr <126159056+Muhammad-Ahmerr@users.noreply.github.com>
2026-02-20 10:28:47 +0000

    1st

---
+++ JS_basic/test.js
@@ -0,0 +1,10 @@
+const pie=3.14
+
+let myName="Ahmer"
+let countryRegion;
+myName="change name"
+// pie=1212
+
+//now a day we didn't use VAR just because of its block scope issue
+
+console.table([myName, countryRegion, pie])
