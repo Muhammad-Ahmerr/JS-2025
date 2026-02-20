4fc72feed06dd645968da6ba05dbc91f70c1c3dd
Muhammad-Ahmerr <126159056+Muhammad-Ahmerr@users.noreply.github.com>
2026-02-20 11:57:44 +0000

    dataType1

---
+++ JS_basic/dataType.js
@@ -0,0 +1,30 @@
+"use strict"; // which is used to make the old code to the newer version of JS
+
+//alert(3+ 3)    // it is mostly used in browser because of integarted Javascript engine in the browser
+
+//most in the code like React we avoid semicolon
+
+//for documentation MDN and https://tc39.es/ecma262/ for official doc
+
+//DataType:
+
+//(Premitve):
+
+//number => 2 to power 52
+//bigInt => more grater value than number like in bitcoin etc
+//String => ""
+//Symbol=> is used to find the uniqueness of the component mostly used in  React
+
+// null => stand alone value => we don't put it like this "" because its not empty
+//undefined => means you declear the the veriable but not declear its value
+//Bolean
+
+
+//Object
+
+console.log(typeof null);  // its a type of object 
+console.log(typeof Object); // its type of function
+
+
+
+
